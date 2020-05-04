68cbdabfc97a734d1893a2b6b4b6d9ab1f11430b
Johnny Villegas <johnvillegas40@gmail.com>
2020-05-04 14:26:23 -0400

    Fixed MongoDB Error

--- models/user.js
+++ models/user.js
@@ -2,18 +2,14 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    emailaddress: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
+    // firstname: String,
+    // lastname: String,
+    // emailaddress: {
+    //     type: String,
+    //     required: true,
+    //     unique: true,
+    // },
+    username: String
 });
 
 
